89ec6ad4b72950b793cad50f087e0f21a9d98350
@khushi'sCodder <palk91190@gmail.com>
2026-03-17 22:48:53 +0530

    Delete day16-project directory

--- day16-project/server.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const app = require("./src/app");
-const DbConnection = require("./src/config/database")
-
-DbConnection();
-app.listen(3000,()=>{
-    console.log("Server is runnig.....")
-})
